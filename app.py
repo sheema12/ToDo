b81326e4f6428801714b9b2d03fa0ce259ad437d
Sheema <sheema.zainab01@gmail.com>
2024-04-21 11:20:15 +0530

    changes

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-print("Start")
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
